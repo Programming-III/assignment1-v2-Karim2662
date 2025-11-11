d76041f50033da9b22c6f6a90688504882db8d3f
karim16006373 <karimalani26@gmail.com>
2025-11-11 09:51:57 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -5,7 +5,21 @@
 #include <string>
 #using namespace std;
 //#write student class here
-
+#ifndef ASSIGMENT01_STUDENT_H
+#define ASSIGMENT01_STUDENT_H
+class Student {
+private:
+    int yearLevel;
+    string major;
+public:
+    Student(int yearLevel, string major);
+    Student();
+    void display();
+    ~Student();
+    int getYearLevel();
+    string getMajor();
+};
+#endif
 
 
 
