6b98da57e40383c22ae28ba1750210a632511a4d
karim16006373 <karimalani26@gmail.com>
2025-11-11 09:53:12 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -5,10 +5,7 @@
 using namespace std;
 
 //#write Person class here
-#ifndef ASSIGMENT01_PERSON_H
-#define ASSIGMENT01_PERSON_H
-#include <string>
-using namespace std;
+
 class Person{
 protected:
     string name;
