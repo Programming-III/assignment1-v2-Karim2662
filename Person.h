fbc47db289342e85120a4c19d739015d8efdd6e6
karim16006373 <karimalani26@gmail.com>
2025-11-11 09:50:19 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -5,7 +5,23 @@
 using namespace std;
 
 //#write Person class here
-
+#ifndef ASSIGMENT01_PERSON_H
+#define ASSIGMENT01_PERSON_H
+#include <string>
+using namespace std;
+class Person{
+protected:
+    string name;
+    int id;
+public:
+    Person(string name,int id);
+    Person();
+    void display();
+    ~Person();
+    int getId();
+    string getName();
+};
+#endif 
 
 
 
