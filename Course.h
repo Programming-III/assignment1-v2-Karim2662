4db04239d88b0aed21d3fd07157c625cd1c8dbfd
karim16006373 <karimalani26@gmail.com>
2025-11-11 09:52:43 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -5,7 +5,28 @@
 #include "Student.h"
 using namespace std;
 //#write your code here
+#ifndef ASSIGMENT01_COURSE_H
+#define ASSIGMENT01_COURSE_H
+#include "Student.h"
 
+class course {
+    private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+public:
+    course(string courseCode, string courseName,int maxStudents, int currentStudents, Student& students);
+    course();
+    void addStudent(const Student& s);
+    void displayCourseInfo();
+    string getCourseCode();
+    string getcourseName();
+    int getMaxStudents();
+    int getCurrentStudents();
+};
+#endif
 
 
 
