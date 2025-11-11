62892c9a08eaee60f57d25be759d44415b7d88b0
karim16006373 <karimalani26@gmail.com>
2025-11-11 09:50:50 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -4,7 +4,20 @@
 #include "Person.h"
 using namespace std;
 //#write Instructor class here
-
+#ifndef ASSIGMENT01_INSTRUCTOR_H
+#define ASSIGMENT01_INSTRUCTOR_H
+class Instructor {
+private:
+    string department;
+    int experienceYears;
+public:
+    Instructor(string department, int experienceYears);
+    Instructor();
+    void display();
+    string getDepartment();
+    int getExperinceYears();
+};
+#endif
 
 
 
