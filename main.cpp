3755edd45733216210332342eb105380a01b6e11
karim16006373 <karimalani26@gmail.com>
2025-12-09 10:01:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -119,16 +119,50 @@ course::~course(){
     delete [] students;
 }
 
+ Student &iostream operator<<(&iostream, int yearLevel, String major){
+  Student<<yearLevel<<major<<endl;
+}
+ Person &iostream operator<<(&iostream, String name, int id){
+  Person<<name<<id<<endl;
+}
+ Instructor &iostream operator<<(&iostream, int yearLevel, String major){
+  Instructor<<department<<experienceYears<<endl;
+}
 
+Course operator[](Student& students){
+  for(int i=0;i<currentStudents){
+    return name;
+  }
+}
 
-
-
-
+ &Course operator+=(const &Student s){
+   currentStudents+=s.major;
+ }
+ friend Course operator==(const &Course c1, const &Course c2){
+    return (c1.courseCode=c2.courseCode);
+ }
+  void Course::roleInfo{
+    return;
+  }
+  
 
 
 // ==================== Main Function ====================
 int main() {
-   
+  int a[]={0,1,2}
+    Instructor ins(computer_science, 5);
+    Student s(2,Informatics);
+    Course c1(Cs101, IntroToInformatics,100,70);
+    Course c2(Cs102,IntroToProg,120,80);
+   cout<<Student<<Person<<Instructor<<endl;
+   roleInfo(ins);
+   roleInfo(s);
+   roleInfo(c1);
+   roleInfo(c2);
+   c1+=(s);
+   display();
+   Course(c1==c2);
+   display();
     
     return 0;
 }
